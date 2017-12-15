10b876d90d46ea69ad0c2a7e0c106de76f3bfd40
Jacopo Scrinzi <scrinzi.jacopo@gmail.com>
2017-12-15 11:58:53 +0000

    add constants to main

--- src/main.py
+++ src/main.py
@@ -6,6 +6,8 @@ from halo import Halo
 import packages
 
 from errors import SetupError
+from constants import MINIMUM_MACOS_VERSION, MINIMUM_MACOS_VERSION, \
+    DOTFILES_ORIGIN, DOTFILES_TARBALL_URL, DOTFILES_DIRECTORY, FILES_TO_SYMLINK
 from helpers import get_os, is_supported_version, print_in_magenta, \
     download_and_untar
 
