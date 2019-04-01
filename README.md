567ec8e5738b7f1a21ba735e352068af5775e270
Jacopo Scrinzi <scrinzi.jacopo@gmail.com>
2019-03-31 21:25:04 -0500

    move back to a bash setup

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ My workenv
 
 ## install
 
-`sh -c "$(curl -fsSL https://raw.githubusercontent.com/eredi93/dotfiles/master/setup.sh)"`
+```
+$ bash -c "$(curl -fsSL https://raw.githubusercontent.com/eredi93/dotfiles/master/setup.sh)"
+```
--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# ----------------------------------------------------------------------
+# | Environment Variables                                              |
+# ----------------------------------------------------------------------
+REPO_DIR=$(dirname "${0}")
+ZSH_PLUGINS="zsh-syntax-highlighting zsh-autosuggestions zsh-history-substring-search"
 
 # ----------------------------------------------------------------------
 # | Helper Functions                                                   |
 # ----------------------------------------------------------------------
-
 ask_for_sudo() {
 
     # Ask for the administrator password upfront.
@@ -23,33 +30,73 @@ ask_for_sudo() {
 
 }
 
-install_python3() {
-  os_name="$(uname -s)"
+# ----------------------------------------------------------------------
+# | Setup  Functions                                                   |
+# ----------------------------------------------------------------------
+setup_zsh() {
+  echo -e ">> Setup ZSH as user Shell\n"
 
-  if command -v python3 &> /dev/null ; then
-    return 0
+  if ! command -v zsh &> /dev/null ; then
+    (>&2 echo "ERROR - ZSH is not installed")
+    exit 1
   fi
 
-  if [ "$os_name" == "Darwin" ]; then
-    if ! command -v brew &> /dev/null ; then
-      echo ">> Installing Brew"
-      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    fi
+  echo "Please enter your Mac user password to setup ZSH:"
+  ask_for_sudo
 
-    echo ">> Installing Python3"
-    brew install python3
-
-    return 0
-  elif [ "$os_name" == "Linux" ] && [ -e "/etc/lsb-release" ]; then
-    echo ">> Installing Python3"
-    sudo apt install -y build-essential libssl-dev libffi-dev python3-dev python3-pip
-
-    return 0
-  else
-    echo "Sorry, this script is intended only for macOS and Ubuntu!"
+  zsh_bin=$(command -v zsh)
+  if ! sudo grep -q "${zsh_bin}" /etc/shells; then
+    sudo sh -c "echo '${zsh_bin}' >> /etc/shells"
   fi
 
-  return 1
+  sudo chsh -s "${zsh_bin}" "${USER}"
+}
+
+create_symbolic_links() {
+  src_dir="${REPO_DIR}/files"
+  dst_dir="${HOME}"
+
+  echo -e ">> Creating symbolic links for files in ${src_dir}\n"
+
+  for file in "${src_dir}"/*; do
+    file_name=$(basename "${file}")
+    dst_file="${dst_dir}/.${file_name}"
+
+    if [ ! -d "$(dirname "${dst_file}")" ]; then mkdir "$(dirname "${dst_file}")"; fi
+    if [ -L "${dst_file}" ]; then rm "${dst_file}"; fi
+    if [ -f "${dst_file}" ]; then mv "${dst_file}" "${dst_file}.local"; fi
+
+    echo "${file} -> ${dst_file}"
+    ln -s "$(realpath "${file}")" "${dst_file}"
+  done
+}
+
+install_oh_my_zsh() {
+  echo -e ">> Installing Oh My ZSH\n"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh -l::g' | sed 's:chsh -s .*$::g')"
+}
+
+oh_my_zsh_plugins() {
+  echo -e ">> Installing Oh My ZSH Plugins\n"
+  for plugin in $ZSH_PLUGINS; do
+    repo="https://github.com/zsh-users/${plugin}.git"
+    dst_dir="${HOME}/.oh-my-zsh/custom/plugins/${plugin}"
+
+    if [ -d "${dst_dir}" ]; then rm -rf "${dst_dir}"; fi
+
+    git clone "${repo}" "${dst_dir}" &> /dev/null
+    echo "${plugin} successfully installed"
+  done
+}
+
+tmux_tpm() {
+  dst_dir="${HOME}/.tmux/plugins/tpm"
+  echo -e ">> Installing Tmux TPM\n"
+
+  if [ -d "${dst_dir}" ]; then rm -rf "${dst_dir}"; fi
+
+  git clone https://github.com/tmux-plugins/tpm "${dst_dir}" &> /dev/null
+  echo "Tmux TPM successfully installed"
 }
 
 setup() {
@@ -57,12 +104,13 @@ setup() {
   # are made relative to this file's path.
   cd "${1}" || exit 1
 
-  install_python3
+  setup_zsh
+  create_symbolic_links
+  install_oh_my_zsh
+  oh_my_zsh_plugins
+  tmux_tpm
 
-  pip3 install -r requirements.txt 2>&1 &> /dev/null
-
-  echo ">> Setup Environment"
-  python3 src/main.py
+  echo "Dotfiles setup completed!"
 }
 
 # ----------------------------------------------------------------------
@@ -70,22 +118,13 @@ setup() {
 # ----------------------------------------------------------------------
 
 main() {
+  dir="${DOTFILES_DIR-$HOME/.dotfiles}"
 
-    ask_for_sudo
+  rm -rf "${dir}"
 
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  git clone git@github.com:eredi93/dotfiles.git "${dir}" &> /dev/null
 
-    dotfiles_dir=$1
-
-    if [ -z $dotfiles_dir ]; then
-        dotfiles_dir="$HOME/.dotfiles"
-    fi
-
-    rm -rf $dotfiles_dir
-
-    git clone git@github.com:eredi93/dotfiles.git $dotfiles_dir &> /dev/null
-
-    setup $dotfiles_dir
+  setup "${dir}"
 }
 
 main "$@"
